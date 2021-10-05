0ca5faf71dad09e5c434f3e4fc167cbe0f14c36f
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-04 22:09:20 -0300

    feat: start project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+// import Header from "header/Header";
+
+const App = () => (
+  <div>
+    {/* <Header /> */}
+    <div>HOST APP</div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
